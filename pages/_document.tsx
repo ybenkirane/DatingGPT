bdfa339ff682f9f4b9f2a2d80f043fcdc981987e
yacinelol24 <yacine.benkirane@mail.mcgill.ca>
2023-03-13 04:00:55 -0400

    Twitter Cards

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,18 +16,18 @@ class MyDocument extends Document {
             content="Generate your next bio in seconds."
           />
           <meta property="og:title" content="DateGPT: AI-Powered Bio Generator" />
-          <meta name="tinder:card" content="summary_large_image" />
-          <meta name="tinder:title" content="DateGPT AI-Powered Bio Generator" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="DateGPT AI-Powered Bio Generator" />
           <meta
-            name="tinder:description"
-            content="Generate your next Tinder bio in seconds."
+            name="twitter:description"
+            content="Generate your next dating bio in seconds."
           />
           <meta
             property="og:image"
             content="https://dategpt.vercel.app/og-image.png"
           />
           <meta
-            name="tinder:image"
+            name="twitter:image"
             content="https://dategpt.vercel.app/og-image.png"
           />
         </Head>
