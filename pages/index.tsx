d8b2b25b99b28a2a58083055a9f24ab0e4206278
yacinelol24 <yacine.benkirane@mail.mcgill.ca>
2023-03-13 02:01:12 -0400

    index bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -163,8 +163,8 @@ const Home: NextPage = () => {
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("2.")
+                  .substring(generatedBios.indexOf("1") + 4)
+                  .split("2.", "3.")
                   .map((generatedBio) => {
                     return (
                       <div
