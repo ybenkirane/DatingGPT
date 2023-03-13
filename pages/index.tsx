33d91d6d859ee870c7a420870971529e0a947fcc
yacinelol24 <yacine.benkirane@mail.mcgill.ca>
2023-03-13 02:05:38 -0400

    ind buhghgg

--- pages/index.tsx
+++ pages/index.tsx
@@ -164,7 +164,8 @@ const Home: NextPage = () => {
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 {generatedBios
                   .substring(generatedBios.indexOf("1") + 4)
-                  .split("2.", "3.")
+                  .split("2.")
+                  .split("3.")
                   .map((generatedBio) => {
                     return (
                       <div
